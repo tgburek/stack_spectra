36c68a8c223aaaa284562e5de81716d9d29ac804
Tim Gburek <tgburek@users.noreply.github.com>
2021-05-19 09:47:55 -0700

    Upon importing, sets plotting parameters of seaborn

---
+++ sns_setstyle.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+
+import seaborn as sns
+
+print ('\nSetting "seaborn" plotting styles\n')
+sns.set_style('white')
+sns.set_style('ticks')
+sns.set_style({"xtick.direction": "in","ytick.direction": "in"})
